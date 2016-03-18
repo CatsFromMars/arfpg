a17f31930383621ee26298aa8cb05e6b12ce79d8
CatsFromMars <mduque@andrew.cmu.edu>
2016-03-18 12:02:17 -0400

    I forgot

--- Assets/scripts/ChalkDogSetup.cs
+++ Assets/scripts/ChalkDogSetup.cs
@@ -7,6 +7,7 @@ public class ChalkDogSetup : MonoBehaviour, GameWinHandler {
   public GameObject dog;
   public GameObject textbox;
 
+  Animator animator;
   TextBox textComponent;
   PetBar pbar;
 
@@ -15,19 +16,29 @@ public class ChalkDogSetup : MonoBehaviour, GameWinHandler {
     textComponent = textbox.GetComponent<TextBox>();
     textComponent.setText("Hi! I'm made of chalk!");
     pbar = bar.GetComponent<PetBar>();
+	animator = dog.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
     if (pbar.isActive && pbar.currentTimeInBounds > 0.5f * pbar.timeToComplete)
     {
+	  animator.SetInteger (Animator.StringToHash("Pet"), 1);
       textComponent.setText("Oh wow, your hands are getting dusty!");
     }
+
+	if (pbar.isActive && pbar.currentTimeInBounds > 0.75f * pbar.timeToComplete)
+	{
+		animator.SetInteger (Animator.StringToHash("Pet"), 2);
+		textComponent.setText("I think some of my chalk is rubbing off...");
+	}
 	}
 
   public void winGame()
   {
-    textComponent.setText("Wow! Good Pet!");
+	animator.SetInteger (Animator.StringToHash("Pet"), 3);
+    textComponent.setText("It's ok, it'll grow back! Great petting!");
     Destroy(bar);
 
     StartCoroutine("endGame");
---
+++ Assets/scripts/DadDogSetup.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DadDogSetup : MonoBehaviour, GameWinHandler {
+
+	public GameObject bar;
+	public GameObject dog;
+	public GameObject textbox;
+	public GameObject gameGUI;
+
+	Animator animator;
+	TextBox textComponent;
+	PetBar pbar;
+
+	// Use this for initialization
+	void Start () {
+		textComponent = textbox.GetComponent<TextBox>();
+		//textComponent.setText("");
+		pbar = bar.GetComponent<PetBar>();
+		animator = dog.GetComponent<Animator> ();
+		gameGUI = GameObject.Find ("Game");
+		StartCoroutine (monologue ());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (pbar.isActive && pbar.currentTimeInBounds > 0.3f * pbar.timeToComplete)
+		{
+			textComponent.setText("You must not pet too much or too little!");
+		}
+	}
+
+	public void winGame()
+	{
+		animator.SetInteger (Animator.StringToHash("Pet"), 1);
+		textComponent.setText("Mmm...You have petted well.");
+		Destroy(bar);
+		gameGUI.SetActive (false);
+		StartCoroutine("endGame");
+	}
+
+	IEnumerator monologue() {
+		gameGUI.SetActive (false);
+		textComponent.setText("Welcome to the World of ARFPG.");
+		yield return new WaitForSeconds(4);
+		textComponent.setText("You have probably never petted a dog before in your life.");
+		yield return new WaitForSeconds(3);
+		textComponent.setText("Fear not, for I shall teach you.");
+		yield return new WaitForSeconds(2);
+		textComponent.setText("Are you ready?");
+		yield return new WaitForSeconds(2);
+		textComponent.setText("To pet a dog, you must click the PET buton in rapid succession.");
+		gameGUI.SetActive (true);
+	}
+
+	IEnumerator endGame()
+	{
+		yield return new WaitForSeconds(2);
+		animator.SetInteger (Animator.StringToHash("Pet"), 0);
+		textComponent.setText("But you still have much to learn, young pettawon.");
+		yield return new WaitForSeconds(4);
+		textComponent.setText("Go forth and fufill your sacred mission...");
+		yield return new WaitForSeconds(4);
+		textComponent.setText("PET ALL DOGS!");
+		yield return new WaitForSeconds(2);
+		Debug.Log("end game things here woo");
+	}
+}
